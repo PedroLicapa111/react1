fec3c2ec49a5bc98458bbb38452ebcd9120405e3
nombre <PedroLic111>
2023-10-15 08:24:39 -0500

    2.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import C01componente from './componentes/C01componente';
 import C04variable from './componentes/C04variable';
+import C02aumentar from './componentes/C02aumentar';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>React</h1>
       <C01componente/>
       <C04variable xVariable="Leer esta variable" />
+      <C02aumentar/>
     </div>
   );
 }
