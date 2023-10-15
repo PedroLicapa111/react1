4c087ffc8843d885908f2d53413aa329efde63ec
nombre <PedroLic111>
2023-10-15 07:18:52 -0500

    2.1 Autodictado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './componentes/C01componente';
+import C04variable from './componentes/C04variable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pedro Licapa <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React</h1>
+      <C01componente/>
+      <C04variable xVariable="Leer esta variable" />
     </div>
   );
 }
---
+++ src/componentes/C01componente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+      Saludo 
+    </div>
+  )
+}
+
+export default C01componente
