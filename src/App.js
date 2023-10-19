d92c233849ac63d082ffd3f59fab7a2832b70a3b
[PedroLicapa111] <[pedro.licapa111@gmail.com]>
2023-10-19 09:19:52 -0500

    S8.2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import C01componente from './componentes/C01componente';
-import C04variable from './componentes/C04variable';
-import C02aumentar from './componentes/C02aumentar';
+
+//import './App.css';
+
 import AppForm from './componentes/AppForm';
 
 function App() {  
   
   return (
-    <div>
-      <h1>React</h1>
-      <C01componente/>
-      <C04variable xVariable="Esta es la variable" />
-      <C02aumentar/>
+    <div style={{background:"yellow", 
+    width:"350px", 
+    padding:"10px"}}>            
       <AppForm/>
     </div>
   );
--- src/componentes/AppForm.js
+++ src/componentes/AppForm.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const AppForm = () => {
   return (
-    <div style={{background:"orange"}}>
+    <div style={{background:"orange", 
+    padding:"10px", 
+    textAlign:"center"}}>
+        <h>AppForm.js</h><br/>
+        <input placeholder="Nombre: "/><br/>
+        <input placeholder="Edad: "/><br/>
+        <input placeholder="Gènero: "/><br/>        
+        <button>Guardar</button>
       
     </div>
   )
